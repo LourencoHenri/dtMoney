74bb866c90e3eac5abc9c2070b13708ab7f7cc5c
Lourenço <henri.lourenco@outlook.com>
2023-07-11 13:28:03 -0300

    [Add] First commit

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,57 @@
+import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
+import { Summary } from "../../components/Summary";
+import {
+	PriceHighlight,
+	TransactionsContainer,
+	TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+	return (
+		<div>
+			<Header />
+			<Summary />
+
+			<TransactionsContainer>
+				<SearchForm />
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>11/07/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Alimentação</td>
+							<td>
+								<PriceHighlight variant="outcome">R$ - 2.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>11/07/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>11/07/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>11/07/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
+		</div>
+	);
+}
